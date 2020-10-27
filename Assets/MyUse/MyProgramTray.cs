5d3b0f2a8c4b8917b88b316276eaec8b15a98be6
VeinsNull <DRshaovx@gmail.com>
2020-10-27 16:07:29 +0800

    v0.1
    实现桌面小宠物及缩小至菜单栏

---
+++ Assets/MyUse/MyProgramTray.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyProgramTray : MonoBehaviour
+{
+#if UNITY_STANDALONE_WIN
+	Tray tray;
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		tray = new Tray();
+		tray.InitTray();
+	}
+	private void OnApplicationQuit()
+	{
+		tray?.Dispose();
+		tray = null;
+	}
+#endif
+}
