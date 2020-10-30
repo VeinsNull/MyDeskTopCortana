d5bbf305d37e4b77cdc96f93422e678a97fed9d0
Veinsvx <DRshaovx@gmail.com>
2020-10-30 15:45:53 +0800

    1.5
    更新

---
+++ Assets/MyUse/ToDoManager.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.Networking;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+public class ToDoManager : MonoBehaviour
+{
+    public InputField todoListInput;//用户输入的任务
+    public Transform content;//存放添加后的任务列表
+    public GameObject ListItemPreFab;//用户任务列表预制体
+
+    string filePath;//存放同步的json文件
+    string jsonName = "1.json";
+    private List<ListObject> ListObjects = new List<ListObject>();
+
+    public class listItemClass
+    {
+        public string objName;
+        public int index;
+        public listItemClass(string name, int index)
+        {
+            this.objName = name;
+            this.index = index;
+        }
+    }
+
+    void Start()
+    {
+        //程序一开始找到json文件目录，并赋值给filepath；
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            filePath = Path.Combine(Application.persistentDataPath, jsonName);
+        }
+        else if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            filePath = @"D:\1.json";
+        }
+        loadJsonData();
+        //然后程序加载本地的json数据
+    }
+
+    public void CreateNewItem()
+    {
+        string temp = todoListInput.text;
+        Debug.Log(temp);
+        CreateListItem(temp);
+        todoListInput.text = "";
+    }
+
+    /// <summary>
+    /// 创建实例化的按钮及更新信息
+    /// 用户点击创建按钮时候，先将数据存到本地，按用户需求上传。
+    /// </summary>
+    /// <param name="temp"></param>
+    /// <param name="loadIndex"></param>
+    /// <param name="loding"></param>
+    private void CreateListItem(string temp, int loadIndex = 0, bool loding = false)
+    {
+        //实例化清单
+        GameObject item = Instantiate(ListItemPreFab);
+        //设为滑动列表的子物体
+        item.transform.SetParent(content);
+        ListObject itemObject = item.GetComponent<ListObject>();
+        int index = 0;
+        if (loding != true)
+        {
+            index = ListObjects.Count;
+        }
+        itemObject.setObjectInfo(temp, index);
+        //为清单上的信息赋值
+        item.GetComponentInChildren<Text>().text = temp;
+        ListObjects.Add(itemObject);
+        ListObject tempItem = itemObject;
+
+        //为button的点击事件添加监听
+        itemObject.transform.Find("abandon").GetComponent<Button>().onClick.AddListener((delegate { CheckItem(tempItem); }));
+        itemObject.transform.Find("complete").GetComponent<Button>().onClick.AddListener((delegate { CheckItem(tempItem); }));
+        if (loding != true)
+        {
+            saveJsonData();
+        }
+    }
+
+    void CheckItem(ListObject item)
+    {
+        Debug.Log("执行删除信息");
+        ListObjects.Remove(item);
+        saveJsonData();
+        Destroy(item.gameObject);
+    }
+
+    /// <summary>
+    /// 从储存清单列表的列表中读取数据写入到json文件中
+    /// </summary>
+    void saveJsonData()
+    {
+        string contents = "";
+        for (int i = 0; i < ListObjects.Count; i++)
+        {
+            listItemClass temp = new listItemClass(ListObjects[i].objName, ListObjects[i].index);
+            contents += JsonUtility.ToJson(temp) + "\n";
+        }
+        File.WriteAllText(filePath, contents);
+    }
+
+    /// <summary>
+    /// 从存储中加载json文件
+    /// </summary>
+    void loadJsonData()
+    {
+        for (int i = 0; i < content.childCount; i++)
+        {
+            Destroy(content.GetChild(i).gameObject);
+        }
+        ListObjects.Clear();
+        string dataAsJson = "";
+        dataAsJson = ReadJsonFun();
+
+        string[] splitContents = dataAsJson.Split('\n');
+        foreach (string content in splitContents)
+        {
+            if (content.Trim() != "")
+            {
+                listItemClass temp = JsonUtility.FromJson<listItemClass>(content.Trim());
+                CreateListItem(temp.objName, temp.index, true);
+            }
+        }
+    }
+
+    public void ButtonClick(string info)
+    {
+        if (info == "下载")
+        {
+            #region 连接服务器
+            IPAddress ip = IPAddress.Parse("108.61.23.214");
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, 2333)); //配置服务器IP与端口  
+                Debug.Log("连接服务器成功:准备下载");
+            }
+            catch
+            {
+                Debug.Log("连接服务器失败");
+                return;
+            }
+            #endregion
+            //封装到函数里由unity的button事件调用
+            clientSocket.Send(Encoding.UTF8.GetBytes(info));//向服务器发送数据，需要发送中文则需要使用Encoding.UTF8.GetBytes()，否则会乱码
+            //发送完下载命令后，准备接收服务端发过来的数据
+            string recvStr = "";
+            byte[] recvBytes = new byte[1024];
+            int bytes;
+            bytes = clientSocket.Receive(recvBytes, recvBytes.Length, 0);    //从服务器端接受返回信息 
+            recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes);
+            //格式化字符串，因为从服务端传过来的数据太乱了
+            string contents = Decodeing(recvStr);
+            contents = contents.Replace(@"\\n", "\r\n");
+            contents = contents.Replace(@"\\", "");
+            contents = contents.Replace(@"\", "");
+            contents = contents.Replace("\'\"","");
+            contents = contents.Replace("\"\'", "");
+            Debug.Log("从服务端获取的数据为：" + contents);
+            File.WriteAllText(filePath, contents);
+            //从本地文件读取重新加载
+            loadJsonData();
+            clientSocket.Close();
+        }
+        else
+        {
+            #region 连接服务器
+            IPAddress ip = IPAddress.Parse("108.61.23.214");
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, 2333)); //配置服务器IP与端口  
+                Debug.Log("连接服务器成功:准备上传");
+            }
+            catch
+            {
+                Debug.Log("连接服务器失败");
+                return;
+            }
+            #endregion
+            //string contents = "";
+            //读取本地json文件，将其转换为string类型
+            string json = ReadJsonFun();
+            Debug.Log(json);
+            //contents = JsonUtility.FromJson<listItemClass>(json);
+            //将string类型的数据发送给服务器，让服务器以json文件保存
+            clientSocket.Send(Encoding.UTF8.GetBytes(json));
+            clientSocket.Close();
+        }
+    }
+
+    string ReadJsonFun()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(filePath);
+        www.SendWebRequest();
+        while (!www.isDone) { }
+        if (string.IsNullOrEmpty(www.error))
+        {
+            string dataAsJson = www.downloadHandler.text;
+            return dataAsJson;
+        }
+        return "";
+    }
+    public string Decodeing(string s)
+    {
+        Regex reUnicode = new Regex(@"\\u([0-9a-fA-F]{4})", RegexOptions.Compiled);
+        return reUnicode.Replace(s, m =>
+        {
+            short c;
+            if (short.TryParse(m.Groups[1].Value, System.Globalization.NumberStyles.HexNumber, CultureInfo.InvariantCulture, out c))
+            {
+                return "" + (char)c;
+            }
+            return m.Value;
+        });
+    }
+}
+
